ff197603f287ac732a9aa28bd1e00ad5d271f768
Ben Ku <benku12803@gmail.com>
2022-12-30 21:38:40 -1000

    implemented entry function to render appropriate page based on URL

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
 
@@ -6,5 +7,15 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    if util.get_entry(title) is not None:
+        return render(request, "encyclopedia/entry.html", {
+            "info": util.get_entry(title),
+            "title": title.title()
+        })
+    return render(request, "encyclopedia/error.html", {
+        "title": title
     })
 
