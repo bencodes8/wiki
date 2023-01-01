18cf0a372fc32187eba9b724d41f44dc3cabc820
Ben Ku <benku12803@gmail.com>
2022-12-31 22:42:29 -1000

    added create path

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
 from . import views
 
 app_name = "encyclopedia"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>".lower(), views.entry, name="<str:title>"),
+    path("create", views.create, name="create"),
+    path("<str:title>".lower(), views.entry, name="entry"),
 ]
+
