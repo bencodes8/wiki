314d897309d515446eddde0af9d0588da6773b57
Ben Ku <benku12803@gmail.com>
2023-01-02 00:13:05 -1000

    added comments for each function and class

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,19 +7,32 @@ import markdown2
 
 from . import util
 
+# class to create a form 
 class NewCreateForm(forms.Form):
     page = forms.CharField(label="New Page Title")
     description = forms.CharField(widget=forms.Textarea(), label="")
 
+# class to create another form for editing entries 
 class EditForm(forms.Form):
     new_description = forms.CharField(widget=forms.Textarea(), label="")
 
+'''
+    index function:
+    ~POST~
+    - users can search pages given a substring of the title of the page
+    - if no pages are found, a message will show 
+    - else display all pages on current page
+    
+    -display all entry pages
+    
+'''
 def index(request):
     if request.method == "POST":
         found = []
         for entry in util.list_entries():
             if entry.lower().find(request.POST["q"].lower()) != -1:
                 index = util.list_entries().index(entry)
+                # appends pages to list if search query is found
                 found += [util.list_entries()[index]]
         return render(request, "encyclopedia/index.html", {
             "entries": found
@@ -30,7 +43,13 @@ def index(request):
         "random" : choice(util.list_entries())
     })
 
-def search(request, title):
+'''
+    entry function:
+    - the route will direct the user based on the title of the route
+    - if the user inputs a title that exists in the entry list, direct the user to the page
+    - else return an error page
+'''
+def entry(request, title):
     if util.get_entry(title) is not None:
         return render(request, "encyclopedia/entry.html", {
             "info": markdown2.markdown(util.get_entry(title)),
@@ -40,7 +59,14 @@ def search(request, title):
         return render(request, "encyclopedia/error.html", {
             "title": title
         })
-
+'''
+    create function:
+    ~ POST ~
+    - user can create a new page by inputting a title and description
+    - if title of page exists, reject it, redirect user back to the create page, and return an error message
+    - else save the new page and redirect user to new page
+    - the new page should also now be added on the index page
+'''
 def create(request):
     if request.method == "POST":
         form = NewCreateForm(request.POST)
@@ -59,7 +85,15 @@ def create(request):
     return render(request, "encyclopedia/create.html", {
         "form": NewCreateForm()
     })
-    
+
+'''
+    edit function:
+    ~ POST ~
+    - users can edit a page
+    - for QOL, the entry description will already prepopulate the textarea
+    - save entry
+    - the user will be redirected to the newly edited page
+'''
 def edit(request, title):
     if request.method == "POST":
         form = EditForm(request.POST)
@@ -72,6 +106,10 @@ def edit(request, title):
         "form": EditForm({'new_description': util.get_entry(title)})
     })
 
+'''
+    /random route
+    redirects user to random entry page
+'''
 def random(request):
     return HttpResponseRedirect(reverse("encyclopedia:entry", kwargs={"title": choice(util.list_entries())}))
 
