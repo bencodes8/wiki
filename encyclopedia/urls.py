af28216b310e51c1dbb34923ddc06111ab96554c
Ben Ku <benku12803@gmail.com>
2023-01-01 23:57:07 -1000

    added a random route to redirect user to random entry page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("create", views.create, name="create"),
     path("<str:title>/edit", views.edit, name="edit"),
+    path("random", views.random, name="random"),
     path("<str:title>".lower(), views.search, name="entry"),
 ]
 
